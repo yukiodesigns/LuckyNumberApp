9fcc1faab1d99f5709446b9bb47dd3fe22e856d7
Yukio <elsie.atieno@student.moringaschool.com>
2024-02-16 09:24:08 +0300

    Declaring

--- app/src/main/java/com/example/luckynumber/LuckyNumberActivity.kt
+++ app/src/main/java/com/example/luckynumber/LuckyNumberActivity.kt
@@ -2,10 +2,16 @@ package com.example.luckynumber
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class LuckyNumberActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_lucky_number)
+
+        val b2 = findViewById<Button>(R.id.btnLucky)
+        val txt1 = findViewById<TextView>(R.id.text1)
+        val luck = findViewById<TextView>(R.id.luckyNum)
     }
 }
--- app/src/main/java/com/example/luckynumber/MainActivity.kt
+++ app/src/main/java/com/example/luckynumber/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.luckynumber
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val b1 = findViewById<Button>(R.id.btn)
+        val txt = findViewById<TextView>(R.id.textView)
+        val editTxt = findViewById<EditText>(R.id.editText)
     }
 }
