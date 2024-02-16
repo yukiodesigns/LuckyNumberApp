eca9fb721b8c6942b00876555c206faace440fdb
Yukio <elsie.atieno@student.moringaschool.com>
2024-02-16 09:51:30 +0300

    Share data

--- app/src/main/java/com/example/luckynumber/LuckyNumberActivity.kt
+++ app/src/main/java/com/example/luckynumber/LuckyNumberActivity.kt
@@ -1,5 +1,6 @@
 package com.example.luckynumber
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -20,18 +21,37 @@ class LuckyNumberActivity : AppCompatActivity() {
         var user_name = receiveUser()
         Toast.makeText(this, "" + user_name, Toast.LENGTH_LONG).show()
 
+        //Random Number
         var random_num = randomNum()
         luck.setText(""+random_num)
+
+        //Sharing
+        b2.setOnClickListener(){
+            shareResults(user_name, random_num)
+        }
+
     }
 
+    //Getting username
     fun receiveUser():String{
         var bundle:Bundle? = intent.extras
         var username = bundle?.get("name").toString()
         return username
     }
 
+    //Random Number
     fun randomNum():Int{
         val random = Random.nextInt(1000)
         return random
+    }
+
+    //Share results
+    fun shareResults(username:String, num:Int){
+        //Implicit
+        var i = Intent(Intent.ACTION_SEND)
+        i.setType("text/plain")
+        i.putExtra(Intent.EXTRA_SUBJECT, "$username is lucky today")
+        i.putExtra(Intent.EXTRA_TEXT, "His lucky number is $num")
+        startActivity(i)
     }
 }
